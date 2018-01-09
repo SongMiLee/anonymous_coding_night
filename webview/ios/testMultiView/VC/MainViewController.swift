c53e208a8da9cdbb09e053f56bccdefe5264ccd8
SongMiLee <s91010@naver.com>
2018-01-09 11:48:50 +0900

    add the indicator

--- webview/ios/testMultiView/VC/MainViewController.swift
+++ webview/ios/testMultiView/VC/MainViewController.swift
@@ -10,26 +10,49 @@ import Foundation
 import UIKit
 import WebKit
 
-class MainViewController: UIViewController, WKUIDelegate{
+class MainViewController: UIViewController, WKNavigationDelegate{
     
     var webView: WKWebView!
-    
+    var indicator: UIActivityIndicatorView!
     override func loadView() {
-        let webConfiguration = WKWebViewConfiguration()
-        webView = WKWebView(frame: .zero, configuration: webConfiguration)
-        webView.uiDelegate = self
-        view = webView
+        self.webView = WKWebView()
+        self.webView.navigationDelegate = self
+        self.webView.sizeToFit()
+        self.view = webView
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
+        //set Init url
         let myURL = URL(string:"https://peaceful-anony-night.herokuapp.com/")
         let myReq = URLRequest(url: myURL!)
-        webView.load(myReq)
+        self.webView.load(myReq)
+    }
+
+    //navigation start
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        indicator = UIActivityIndicatorView()
+        indicator.color = UIColor(red: 95.0/255.0, green: 75.0/255.0, blue: 139.0/255.0, alpha: 1.0)
+        indicator.frame = CGRect(x: view.frame.midX - 25, y: view.frame.midY - 25 , width: 50, height: 50)
+        indicator.hidesWhenStopped = true
+        indicator.startAnimating()
+        
+        self.view.addSubview(indicator)
+        self.view.bringSubview(toFront: indicator)
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    //navigation complete
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.indicator.removeFromSuperview()
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 }
